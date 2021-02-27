9f3eb4d604309879543dbcb71eeb9462452c06eb
Marco Bronzini <marco.bronzini@outlook.it>
2021-02-27 14:56:20 +0100

    Added some comments about the overfitting phenomenon

--- recom_system.py
+++ recom_system.py
@@ -2,27 +2,29 @@ import utils
 from sklearn.model_selection import train_test_split
 from ExplicitMF import ExplicitMF as ExplicitMatrixFactorization
 
+# Useful libraries for RS:
+# a) sklearn.feature_extraction (e.g. TfidfVectorizer)
+# b) sklearn.metrics.pairwise (e.g. cosine_similairty)
+
 
 def collaborative_filtering(ratings):
     # Split the dataset into training and test sets
     train_events, test_events = train_test_split(ratings, test_size=0.2, random_state=99)
 
     # Run the Algorithm of Explicit Matrix Factorization
-    mf = ExplicitMatrixFactorization(train_events, verbose=True)
-    predictions = mf.predictions
+    model = ExplicitMatrixFactorization(train_events, n_factors=40, item_reg=1.0, user_reg=1.0,
+                                        verbose=True)
 
     # Test it on unseen data (train set)
     iter_array = [1, 2, 5, 10, 25, 50, 100]
-    mf.calculate_learning_curve(iter_array, test_events)
+    model.calculate_learning_curve(iter_array, test_events)
 
     # Plot
-    mf.plot_learning_curve(iter_array)
-
-    return predictions
+    model.plot_learning_curve(iter_array)
+    return model
 
 
 if __name__ == "__main__":
-
     # Import dataset
     df = utils.import_dataset("./data")
     print("EXAMPLE of an event:\n", df.loc[1000, :])
@@ -30,11 +32,18 @@ if __name__ == "__main__":
     # Create the User-Item matrix
     ratings = utils.Dataframe2UserItemMatrix(df)
 
-    # Method 1: Collaborative filtering through the "Explicit Matrix Factorization" algorithm
+    # METHOD 1: Model-based collaborative filtering
+    # Latent factors: Explicit Matrix Factorization
     print("\n1) Recommendation based on the CF method (Matrix Factorization) ...")
-    recommendations_cf = collaborative_filtering(ratings)
-    print("\nRECOMMENDATIONS \n", recommendations_cf)
+    model_cf = collaborative_filtering(ratings)
+    print("\nPREDICTIONS \n", model_cf.predictions)
 
+    # TODO tackle the overfittig phenomenon
+    # ISSUE: Huge difference between training and test data)
+    # HOW: Try to follow this guide (from "Evaluation and Tuning")
+    # - https://www.ethanrosenthal.com/2016/01/09/explicit-matrix-factorization-sgd-als/
+    # - NB: sklearn.model_selection.GridSearchCV could be useful
+    # - Could be useful SGD: https://scikit-learn.org/stable/modules/sgd.html
 
-
-
+    # ALTERNATIVE: Implement Matrix factorization through a specific library
+    # https://pypi.org/project/matrix-factorization/
